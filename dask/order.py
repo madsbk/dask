888dcdb0de8ea2c53084b53c74ee02b01b242646
Mads R. B. Kristensen <madsbk@gmail.com>
2020-03-31 06:58:31 -0700

    order(): added hack to priorities "shuffle-split"

--- dask/order.py
+++ dask/order.py
@@ -489,6 +489,15 @@ def order(dsk, dependencies=None):
             item = init_stack_pop()
         inner_stacks_append([item])
 
+    # TODO: Hack to priorities "shuffle-split"
+    shuffle_split_keys = []
+    for k in result.keys():
+        if len(k) > 0 and "shuffle-split" in k[0]:
+            shuffle_split_keys.append(k)
+
+    for k in shuffle_split_keys:
+        result[k] = 0
+
     return result
 
 
